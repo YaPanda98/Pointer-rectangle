97413594eb1636daa8b77d35cc1e8ede485624f8
YaPanda98 <52866775+YaPanda98@users.noreply.github.com>
2019-07-18 21:29:22 +0300

    Add files via upload

---
+++ Rectangle/src/Rect.java
@@ -0,0 +1,14 @@
+
+public class Rect {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Rectangle rect = new Rectangle(6,  8);
+rect.countArea();
+rect.countPerimeter();
+	}
+}
+
+
+
+	
